fc17a9e50b992263981a6aebcced4e6a25e06e69
MrMelon54 <github@mrmelon54.com>
2022-11-12 18:43:15 +0000

    Start writing ws handler

--- ws.go
+++ ws.go
@@ -1,18 +1,67 @@
 package prosperity_r_place
 
 import (
+	"bufio"
 	"github.com/gorilla/websocket"
+	"image/color"
+	"strconv"
 )
 
 func HandleWebsocket(conn *websocket.Conn) {
 	defer func(conn *websocket.Conn) {
 		_ = conn.Close()
 	}(conn)
+outer:
 	for {
 		messageType, r, err := conn.NextReader()
 		if err != nil {
 			return
 		}
-		conn.NextWriter()
+		if messageType == websocket.BinaryMessage {
+			// no binary messages
+			break
+		}
+
+		scanner := bufio.NewScanner(r)
+		scanner.Split(bufio.ScanWords)
+
+		var x, y int64
+		var colour color.RGBA
+		i := 0
+		for scanner.Scan() {
+			switch i {
+			case 0:
+				x, err = strconv.ParseInt(scanner.Text(), 10, 64)
+				if err != nil {
+					break outer
+				}
+			case 1:
+				y, err = strconv.ParseInt(scanner.Text(), 10, 64)
+				if err != nil {
+					break outer
+				}
+			case 2:
+
+			}
+		}
+
+		// read X coordinate string
+		xStr, err := rBuf.ReadString(',')
+		if err != nil {
+			// bad packet
+			break
+		}
+
+		// read Y coordinate string (
+		yStr, err := rBuf.ReadString(',')
+		if err != nil {
+			return
+		}
+
+		// read colour string (line ends with \n)
+		colourStr, err := rBuf.ReadString('\n')
+		if err != nil {
+			return
+		}
 	}
 }
